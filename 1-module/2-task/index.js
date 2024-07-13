e8315b6bcadec99bf43e24f7d958029deb808b6c
Aziz <mikasa1353@gmail.com>
2024-07-13 11:50:38 +0600

    task-2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Эту функцию трогать не нужно
  */
@@ -10,15 +11,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name === null || name.includes(" ") || name.length < 4) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
