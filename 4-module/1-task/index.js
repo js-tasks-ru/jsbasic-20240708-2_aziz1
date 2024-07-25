932230779f6c7e2b85e48869253dc4e1532626dc
Aziz <mikasa1353@gmail.com>
2024-07-25 16:49:07 +0600

    M4task-1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
+"use strict";
+
 function makeFriendsList(friends) {
-  // ваш код...
+  let body = document.querySelector("body");
+  let ul = document.createElement("ul");
+  let arr = friends.map((friend) => friend.firstName + " " + friend.lastName);
+  for (let elem of arr) {
+    let li = document.createElement("li");
+    li.innerHTML = elem;
+    ul.append(li);
+  }
+
+  body.append(ul);
 }
