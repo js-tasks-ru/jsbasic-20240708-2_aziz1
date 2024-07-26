bca0ec4ab7dffec5be63a90d95a79111b9067b04
Aziz <mikasa1353@gmail.com>
2024-07-26 16:21:41 +0600

    M4task-2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
+"use strict";
 function makeDiagonalRed(table) {
   // ваш код...
+  let row = table.rows;
+  for (let i = 0; i < row.length; i++) {
+    row[i].cells[i].style.backgroundColor = "red";
+  }
+
+  return row;
 }
