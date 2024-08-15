c4aec9f66a17ec57ad9a5aedf14099e2da1d06cd
Aziz <mikasa1353@gmail.com>
2024-08-15 13:41:12 +0600

    m8task2

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,57 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+    this.renderContent();
+  }
+
+  render() {
+    this.elem = createElement(`<div class="products-grid">
+      <div class="products-grid__inner">
+
+      </div>
+    </div>`);
+    let productsGridInner = this.elem.querySelector(".products-grid__inner");
+    this.products.forEach((element) => {
+      let productCard = new ProductCard(element);
+      productsGridInner.append(productCard.elem);
+    });
+  }
+
+  renderContent() {
+    let productsGridInner = this.elem.querySelector(".products-grid__inner");
+    productsGridInner.innerHTML = "";
+    for (let product of this.products) {
+      if (this.filters.noNuts && product.nuts) {
+        continue;
+      }
+
+      if (this.filters.vegeterianOnly && !product.vegeterian) {
+        continue;
+      }
+
+      if (
+        this.filters.maxSpiciness !== undefined &&
+        product.spiciness > this.filters.maxSpiciness
+      ) {
+        continue;
+      }
+
+      if (this.filters.category && product.category != this.filters.category) {
+        continue;
+      }
+
+      let card = new ProductCard(product);
+      productsGridInner.append(card.elem);
+    }
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.renderContent();
   }
 }
